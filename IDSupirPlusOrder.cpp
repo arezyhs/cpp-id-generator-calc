ac2cb22c87bc832393e9b0952c770565e502fd9b
Akbar Setiyawan <akbar.23335@mhs.unesa.ac.id>
2024-03-11 22:27:06 +0700

    Update IDSupirPlusOrder.cpp
    updated the codes.

--- IDSupirPlusOrder.cpp
+++ IDSupirPlusOrder.cpp
@@ -6,7 +6,8 @@
 #include <ctime>
 using namespace std;
 
-// ID Generator (FIX)
+// arezyh.s
+// generator ID untuk supir baru/update supir.
 string buatIDBaru(string nama, char gender, string tanggal_lahir, unordered_set<string>& duplikasiID) {
     int digitDuaPertama = abs((tolower(nama[0]) - 'a' + 1) - (tolower(nama[nama.length() - 1]) - 'a' + 1));
     int digitKetiga = (gender == 'L') ? 1 : 0;
@@ -20,12 +21,10 @@ string buatIDBaru(string nama, char gender, string tanggal_lahir, unordered_set<
                    to_string(digitKetiga) +
                    to_string(digitKeempat) +
                    to_string(digitKelima);
-
-        // Menambahkan leading zero jika jumlah digit kurang dari 5
+        // + 0 di awal jika < 5 digit
         if (hasilID.length() < 5) {
             hasilID = string(5 - hasilID.length(), '0') + hasilID;
         }
-
         digitKelima++;
 
         if (digitKelima > 99) {
@@ -36,27 +35,26 @@ string buatIDBaru(string nama, char gender, string tanggal_lahir, unordered_set<
     } while (duplikasiID.find(hasilID) != duplikasiID.end());
 
     duplikasiID.insert(hasilID);
-
-    // Pindahkan cout ke sini agar ID ditampilkan dengan benar
-    cout << "ID SUPIR   : " << hasilID << endl;
+    cout << "ID Supir   : " << hasilID << endl;
 
     return hasilID;
 }
 
+// generator ID untuk order supir.
 string buatIDOrder(char platNomor, string idSupir, string tujuan, string namaPelanggan) {
-    int urutanAlphabet = tolower(platNomor) - 'a' + 1;
+    int digitOrderDuaPertama = tolower(platNomor) - 'a' + 1;
     int digitIDSupir = stoi(idSupir) ;
-    int urutanTujuan = 0;
+    int digitTujuan = 0;
 
     if (tujuan.length() >= 2) {
         char lastChar1 = tolower(tujuan[tujuan.length() - 2]);
         char lastChar2 = tolower(tujuan[tujuan.length() - 1]);
-        urutanTujuan = (lastChar1 - 'a' + 1) + (lastChar2 - 'a' + 1);
+        digitTujuan = (lastChar1 - 'a' + 1) + (lastChar2 - 'a' + 1);
     } else {
         cerr << "Error: MINIMAL HARUS 2 CHAR PADA DESTINASI TUJUAN!" << endl; // buat test
         exit(EXIT_FAILURE);
     }
-
+    // arezyh.s
     int jumlahHurufNama = 0;
     for (char huruf : namaPelanggan) {
         if (isalpha(huruf)) {
@@ -66,32 +64,31 @@ string buatIDOrder(char platNomor, string idSupir, string tujuan, string namaPel
 
     int digitTerakhir = jumlahHurufNama % 10;
 
-    // Format ID order
-    return to_string(urutanAlphabet) +
+    // format order ID menjadi 10 digit.
+    return to_string(digitOrderDuaPertama) +
            // Pad digitIDSupir with leading zeros to make it a 5-digit number
            to_string(digitIDSupir / 10000) +
            to_string(digitIDSupir % 10000) +
-           to_string(urutanTujuan) +
+           to_string(digitTujuan) +
            to_string(digitTerakhir);
 }
-
 
 // TEST DATA SUPIR DAN PELANGGAN DI SINI!!!!!!! UBAH DATANYA SENDIRI!!!!
 int main() {
     unordered_set<string> duplikasiID;  // Untuk melacak ID yang telah dibuat
 
     // UBAH DATA SUPIR DI BAWAH!!!
-    string namaSupir = "Sugeng Cihuy";
+    string namaSupir = "Akbar Setiyawan"; // Change the name of the driver
     char genderSupir = 'L';
-    string tanggalLahirSupir = "21-02-1999";
+    string tanggalLahirSupir = "27-10-2002";
     string userID = buatIDBaru(namaSupir, genderSupir, tanggalLahirSupir, duplikasiID);
 
     // UBAH DATA PELANGGAN DI BAWAH
-    char platNomor = 'L';
-    string idSupir = userID; // ambil dari ID SUPIR
+    char platNomor = 'L'; // L 1996 YZ
+    string idSupir = userID; // scrap dari ID SUPIR
     string tujuan = "Ketintang";
     string namaPelanggan = "Komeng";
-
+    // arezyh.s
     string orderID = buatIDOrder(platNomor, idSupir, tujuan, namaPelanggan);
     cout << "ID Order   : " << orderID << endl;
 
