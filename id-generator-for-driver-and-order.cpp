df3511326b5c577a009125ab84d33d747dd106b6
Akbar Setiyawan <akbar.23335@mhs.unesa.ac.id>
2024-03-18 05:27:12 +0700

    Update id-generator-for-driver-and-order.cpp
    cek sendiri

--- id-generator-for-driver-and-order.cpp
+++ id-generator-for-driver-and-order.cpp
@@ -8,15 +8,19 @@ using namespace std;
 
 // arezyh.s
 // generator ID untuk supir baru/update supir.
-string buatIDBaru(string nama, char gender, string tanggal_lahir, unordered_set<string>& duplikasiID) {
+string generatorIDSupir(string nama, char gender, string tanggal_lahir, unordered_set<string>& duplikasiID) {
+    // nama depan nama - nama akhir kan?
     int digitDuaPertama = abs((tolower(nama[0]) - 'a' + 1) - (tolower(nama[nama.length() - 1]) - 'a' + 1));
+    // kalau L = 1, kalau P = 0.
     int digitKetiga = (gender == 'L') ? 1 : 0;
+    // angka terakhir dari DD-MM-YYYY dijumlah, terus lanjut mod 9.
     int digitKeempat = ((tanggal_lahir[1] - '0') + (tanggal_lahir[4] - '0') + (tanggal_lahir[9] - '0')) % 9;
-
+    // mulai dari nol ya, naik kalau ada duplikasi.
     int digitKelima = 0;
-    string hasilID;
 
+    string hasilID; // buat nyimpen ID supir.
     do {
+        // digabungin semua tuh...
         hasilID = to_string(digitDuaPertama) +
                    to_string(digitKetiga) +
                    to_string(digitKeempat) +
@@ -25,15 +29,10 @@ string buatIDBaru(string nama, char gender, string tanggal_lahir, unordered_set<
         if (hasilID.length() < 5) {
             hasilID = string(5 - hasilID.length(), '0') + hasilID;
         }
-        digitKelima++;
-
-        if (digitKelima > 99) {
-            cerr << "Error: Infinite loop detected. Unable to generate a unique ID." << endl;
-            exit(EXIT_FAILURE);
-        }
+        digitKelima++; // digitKelimat +1 kalau ada duplikasi.
 
     } while (duplikasiID.find(hasilID) != duplikasiID.end());
-
+// arezyh.s
     duplikasiID.insert(hasilID);
     cout << "ID Supir   : " << hasilID << endl;
 
@@ -41,36 +40,36 @@ string buatIDBaru(string nama, char gender, string tanggal_lahir, unordered_set<
 }
 
 // generator ID untuk order supir.
-string buatIDOrder(string platNomor, string idSupir, string tujuan, string namaPelanggan) {
-    // Mengonversi karakter pertama plat nomor menjadi dua digit
-    int digitPlatNomor = (tolower(platNomor[0]) - 'a' + 1);
-    string digitPlatNomorStr = (digitPlatNomor < 10) ? "0" + to_string(digitPlatNomor) : to_string(digitPlatNomor);
-    int digitIDSupir = stoi(idSupir);
-    int digitTujuan = 0;
+string generatorIDOrder(string platNomor, string idSupir, string tujuan, string namaPelanggan) {
+    // mendapatkan nilai dari karakter pertama platNomor!
+    int digitPlatNomor = (tolower(platNomor[0]) - 'a' + 1); // untuk digit 1 dan 2
+    // kalau nilai karakternya <10, tambahin nol. Misal A = 1, jadinya 01.
+    string digitPlatNomorStr = (digitPlatNomor < 10) ? "0" + to_string(digitPlatNomor) : to_string(digitPlatNomor); // untuk digit ke 3 4 5 6 7
+    // mendapatkan ID supir. klasik...
+    string digitIDSupirStr = (idSupir.length() < 5) ? string(5 - idSupir.length(), '0') + idSupir : idSupir;
 
+    int digitTujuan = 0; // digit 8 9
     if (tujuan.length() >= 2) {
         char lastChar1 = tolower(tujuan[tujuan.length() - 2]);
         char lastChar2 = tolower(tujuan[tujuan.length() - 1]);
         digitTujuan = (lastChar1 - 'a' + 1) + (lastChar2 - 'a' + 1);
     } else {
-        cerr << "Error: MINIMAL HARUS 2 CHAR PADA DESTINASI TUJUAN!" << endl; // buat test
+        // ini gak usah dicopas juga ya!
+        cerr << "Error: MINIMAL HARUS ADA DUA HURUF PADA DESTINASI TUJUAN!" << endl;
         exit(EXIT_FAILURE);
     }
-
-    int jumlahHurufNama = 0;
+// arezyh.s
+    int jumlahHurufNama = 0; // digit 10
     for (char huruf : namaPelanggan) {
         if (isalpha(huruf)) {
             jumlahHurufNama += tolower(huruf) - 'a' + 1;
         }
     }
-
     int digitTerakhir = jumlahHurufNama % 10;
 
-    // format order ID menjadi 10 digit.
+    // gabungin semuanya menjadi 10 digit ID order.
     return digitPlatNomorStr +
-           // Pad digitIDSupir with leading zeros to make it a 5-digit number
-           to_string(digitIDSupir / 10000) +
-           to_string(digitIDSupir % 10000) +
+           digitIDSupirStr +
            to_string(digitTujuan) +
            to_string(digitTerakhir);
 }
@@ -81,18 +80,18 @@ int main() {
     unordered_set<string> duplikasiID;  // Untuk melacak ID yang telah dibuat
 
     // UBAH DATA SUPIR DI BAWAH!!!
-    string namaSupir = "Akbar Setiyawan"; // ubah nama supir di sini
+    string namaSupir = "Akbar SetiyawA"; // ubah nama supir di sini
     char genderSupir = 'L'; // ubah gender supir 'L' atau 'P' di sini
     string tanggalLahirSupir = "27-10-2002"; // ubah tanggalLahirSupir dengan formad DD-MM-YYYY di sini.
-    string userID = buatIDBaru(namaSupir, genderSupir, tanggalLahirSupir, duplikasiID);
+    string userID = generatorIDSupir(namaSupir, genderSupir, tanggalLahirSupir, duplikasiID);
 
     // UBAH DATA PELANGGAN DI BAWAH
-    string platNomor = "AE"; // ubah plat nomor di sini
-    string idSupir = userID;
+    string platNomor = "AE"; // ubah plat nomor di sini, bebas! kan cuma diambil huruf pertama -_-
+    string idSupir = userID; // ambil dari userID langsung
     string tujuan = "Ketintang"; // ubah tujuan di sini
     string namaPelanggan = "Komeng"; // ubah nama pelanggan di sini
     // arezyh.s
-    string orderID = buatIDOrder(platNomor, idSupir, tujuan, namaPelanggan);
+    string orderID = generatorIDOrder(platNomor, idSupir, tujuan, namaPelanggan);
     cout << "ID Order   : " << orderID << endl;
 
     return 0;
