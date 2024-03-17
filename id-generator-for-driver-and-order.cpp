59579e853ff6693a1372d392d25ce1354d1fb747
Akbar Setiyawan <akbar.23335@mhs.unesa.ac.id>
2024-03-18 05:45:16 +0700

    Update id-generator-for-driver-and-order.cpp
    cek komeng lagi

--- id-generator-for-driver-and-order.cpp
+++ id-generator-for-driver-and-order.cpp
@@ -18,43 +18,45 @@ string generatorIDSupir(string nama, char gender, string tanggal_lahir, unordere
     // mulai dari nol ya, naik kalau ada duplikasi.
     int digitKelima = 0;
 
-    string hasilID; // buat nyimpen ID supir.
+    string IDSupir; // buat nyimpen ID supir.
     do {
         // digabungin semua tuh...
-        hasilID = to_string(digitDuaPertama) +
+        IDSupir = to_string(digitDuaPertama) +
                    to_string(digitKetiga) +
                    to_string(digitKeempat) +
                    to_string(digitKelima);
         // + 0 di awal jika < 5 digit
-        if (hasilID.length() < 5) {
-            hasilID = string(5 - hasilID.length(), '0') + hasilID;
+        if (IDSupir.length() < 5) {
+            IDSupir = string(5 - IDSupir.length(), '0') + IDSupir;
         }
         digitKelima++; // digitKelimat +1 kalau ada duplikasi.
 
-    } while (duplikasiID.find(hasilID) != duplikasiID.end());
+    } while (duplikasiID.find(IDSupir) != duplikasiID.end());
 // arezyh.s
-    duplikasiID.insert(hasilID);
-    cout << "ID Supir   : " << hasilID << endl;
-
-    return hasilID;
+    duplikasiID.insert(IDSupir);
+    cout << "ID Supir   : " << IDSupir << endl;
+    return IDSupir;
 }
 
 // generator ID untuk order supir.
 string generatorIDOrder(string platNomor, string idSupir, string tujuan, string namaPelanggan) {
     // mendapatkan nilai dari karakter pertama platNomor!
     int digitPlatNomor = (tolower(platNomor[0]) - 'a' + 1); // untuk digit 1 dan 2
-    // kalau nilai karakternya <10, tambahin nol. Misal A = 1, jadinya 01.
+    // kalau nilai karakternya <10, tambahin nol di awal. Misal A = 1, jadinya 01.
     string digitPlatNomorStr = (digitPlatNomor < 10) ? "0" + to_string(digitPlatNomor) : to_string(digitPlatNomor);
     // mendapatkan ID supir. klasik...
-    string digitIDSupirStr = (idSupir.length() < 5) ? string(5 - idSupir.length(), '0') + idSupir : idSupir; // untuk digit ke 3 4 5 6 7
+    string digitIDSupirStr = (idSupir.length() < 5) ? string(5 - idSupir.length(), '0') + idSupir : idSupir; // untuk digit 3 4 5 6 7
 
-    int digitTujuan = 0; // digit 8 9
+    int digitTujuan = 0; // untuk digit 8 9
+    // kalau string tujuannya >= 2, dia jalan
     if (tujuan.length() >= 2) {
         char lastChar1 = tolower(tujuan[tujuan.length() - 2]);
         char lastChar2 = tolower(tujuan[tujuan.length() - 1]);
         digitTujuan = (lastChar1 - 'a' + 1) + (lastChar2 - 'a' + 1);
-    } else {
+    } else { // kalau <2 programnya mokad, misal cuma masukin 'A' doang atau 'Z' doang.
         // ini gak usah dicopas juga ya!
+        // daripada dibuat gini, mending ketika user masukin tujuan, dibuat perulangan aja sampe si user masukin minimal 2 huruf
+        // atau tambahin nol di awal jika <10.
         cerr << "Error: MINIMAL HARUS ADA DUA HURUF PADA DESTINASI TUJUAN!" << endl;
         exit(EXIT_FAILURE);
     }
@@ -65,7 +67,7 @@ string generatorIDOrder(string platNomor, string idSupir, string tujuan, string 
             jumlahHurufNama += tolower(huruf) - 'a' + 1;
         }
     }
-    int digitTerakhir = jumlahHurufNama % 10;
+    int digitTerakhir = jumlahHurufNama % 10; // ambil angka terakhir
 
     // gabungin semuanya menjadi 10 digit ID order.
     return digitPlatNomorStr +
@@ -74,20 +76,21 @@ string generatorIDOrder(string platNomor, string idSupir, string tujuan, string 
            to_string(digitTerakhir);
 }
 
-
 // TEST DATA SUPIR DAN PELANGGAN DI SINI!!!!!!! UBAH DATANYA SENDIRI!!!!
 int main() {
-    unordered_set<string> duplikasiID;  // Untuk melacak ID yang telah dibuat
+    unordered_set<string> duplikasiID;  // simpen ID yang udah jadi di sini.
 
     // UBAH DATA SUPIR DI BAWAH!!!
     string namaSupir = "Akbar SetiyawA"; // ubah nama supir di sini
     char genderSupir = 'L'; // ubah gender supir 'L' atau 'P' di sini
     string tanggalLahirSupir = "27-10-2002"; // ubah tanggalLahirSupir dengan formad DD-MM-YYYY di sini.
-    string userID = generatorIDSupir(namaSupir, genderSupir, tanggalLahirSupir, duplikasiID);
+    string IDSupir = generatorIDSupir(namaSupir, genderSupir, tanggalLahirSupir, duplikasiID);
+    cout << "ID Supir   : " << IDSupir << endl;
+
 
     // UBAH DATA PELANGGAN DI BAWAH
-    string platNomor = "AE"; // ubah plat nomor di sini, bebas! kan cuma diambil huruf pertama -_-
-    string idSupir = userID; // ambil dari userID langsung
+    string platNomor = "AE"; // ubah plat nomor di sini
+    string idSupir = IDSupir; //ambil dari IDSupir langsung
     string tujuan = "Ketintang"; // ubah tujuan di sini
     string namaPelanggan = "Komeng"; // ubah nama pelanggan di sini
     // arezyh.s
